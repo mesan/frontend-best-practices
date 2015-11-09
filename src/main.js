aeaeb44f524105cc4ad16973081b82a514493680
Christian Gjerde Jensen <me@cgj.me>
2015-11-09 15:44:53 +0100

    tweak

--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@ import printerController from "./js/printer";
 
 (function () {
   const printer = printerController();
-
-  const obj = { key: 42 };
-
-  // tests
+  const obj = {
+    key: 42
+  };
   if (obj) {
     obj.key++;
   }
 
   printer.hello("world", () => 42);
 })();
+
+const getFullName = ({ firstName, lastName = "Jensen" }) => {
+  return `${firstName} ${lastName}`;
+};
+
+const person = { age: 26, firstName: "Christian" };
+getFullName(person);
